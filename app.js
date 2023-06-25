cfbcba3c357cb0300d52535020e561a7787d1d1d
quenten8 <quenten.cox00@outlook.com>
2023-06-24 20:30:59 -0700

    Initial commit

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./path/swagger-output.json')
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const app = express();
 const port = process.env.PORT || 8080
 
-app.use(bodyParser.json());
-app.use('/', require('./routes'))
+// Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-mongodb.connect((err, mongodb) => {
+app.use(bodyParser.json());
+app.use('/', require('./routes'));
+
+
+
+mongodb.connect((err, mongodb) => { 
     if(err) {
         console.log(err);
     } else {
