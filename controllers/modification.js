ad62dd4d500c04acd7928086bf26edeb1f5f07d1
Eli Cutchen <65992004+ejctchn@users.noreply.github.com>
2023-07-01 17:32:37 -0600

    Modification Controller complete

--- controllers/modification.js
+++ controllers/modification.js
@@ -4,43 +4,190 @@ const ObjectId = require('mongodb').ObjectId;
 async function getAllMod(req, res, next)
 {
     // #swagger.tags = ['Modifications']
-    res.send('get all modifications');
+
+    try
+    {
+        const result = await mongo
+            .getConnection()
+            .db('flavor-hub')
+            .collection('modification')
+            .find();
+        result.toArray().then((lists) => 
+        {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists);
+        });
+    }
+    catch(error)
+    {
+        res.status(400).json({message:"Get all mods error, unexpected"})
+    }
 }
 
 async function getModById(req, res, next)
 {
     // #swagger.tags = ['Modifications']
-    res.send('get modification by mod id');
+
+    if(!ObjectId.isValid(req.params.modId))
+    {
+        ('Invalid ID');
+
+        const modId = new ObjectId(req.params.modId);
+        const result = await mongo
+            .getConnection()
+            .db('flavor-hub')
+            .collection('modification')
+            .find({_id: modId});
+        result.toArray().then((lists) => 
+        {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists[0]);
+        });
+    } 
 }
 
 async function getModByUserId(req, res, next)
 {
     // #swagger.tags = ['Modifications']
-    res.send('get modification by user id');
+
+    if (!ObjectId.isValid(req.params.userId)) 
+    {
+        ('Invalid ID');
+    
+    
+        const userId = new ObjectId(req.params.userId);
+        const result = await mongo
+            .getConnection()
+            .db('flavor-hub')
+            .collection('modification')
+            .find({userId: userId});
+        result.toArray().then((lists) => 
+        {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists[0]);
+        });
+
+    }
 }
 
-async function addNewMod(req, res, next)
+async function addNewMod(collection, userId, type)
 {
     // #swagger.tags = ['Modifications']
-    res.send('create new mod');
+
+    const d = new Date();
+    const day = d.getDate();
+    const month = d.getMonth() + 1;
+    const year = d.getFullYear();
+    let today_date = `${month}/${day}/${year}`;
+
+    const modified =
+    {
+        date: today_date,
+        modified_collection: collection,
+        modified_type: type,
+        userId: userId
+    };
+    const mod = await mongo
+        .getConnection()
+        .db('flavor-hub')
+        .collection('modification')
+        .insertOne(modified);
 }
 
 async function updateModById(req, res, next)
 {
     // #swagger.tags = ['Modifications']
-    res.send('update modification by mod id');
+
+    try
+    {
+        if (!ObjectId.isValid(req.params.modId)) 
+        {
+            throw new Error('Invalid ID');
+        }
+
+        const modId = new ObjectId(req.params.modId);
+        
+        const modified = {
+            date: req.body.today_date,
+            modified_collection: req.body.collection,
+            modified_type: req.body.type,
+            userId: req.body.userId
+        };
+        const response = await mongo
+            .getConnection()
+            .db('flavor-hub')
+            .collection('modification')
+            .replaceOne({ _id: modId }, modified);
+        console.log(response);
+        if (response.modifiedCount > 0)
+        {
+            res.status(204).send();
+            addNewMod("Modifications", "123456", "Update Modifications");
+        } 
+        else 
+        {
+            res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+        }
+    }
+    catch(error)
+    {
+        res.status(400).json({message:"Update ModById error, unexpected"});
+    }
 }
 
 async function deleteModById(req, res, next)
 {
     // #swagger.tags = ['Modifications']
-    res.send('delete modification by mod id');
+
+    try
+    {
+        if (!ObjectId.isValid(req.params.modId)) 
+        {
+            throw new Error('Invalid ID');
+        }
+        const modId = new ObjectId(req.params.modId);
+        const response = await mongo
+        .getConnection()
+        .db('flavor-hub')
+        .collection('modification')
+        .deleteOne({ _id: modId }, true);
+        console.log(response);
+        if (response.deletedCount > 0) 
+        {
+            res.status(200).send();
+            modify();
+        } 
+        else 
+        {
+            res.status(500).json(response.error || 'Some error occurred while deleting the mod.');
+        }
+    }
+    catch(error)
+    {
+        res.status(400).json({message:error.message});
+    }
 }
 
 async function getModByType(req, res, next)
 {
     // #swagger.tags = ['Modifications']
-    res.send('get modification by recipe category');
+
+    if (!ObjectId.isValid(req.params.modified_type)) 
+    {
+        ('Invalid type ID');
+        
+        const typeId = new ObjectId(req.params.modified_type);
+        const result = await mongo
+            .getConnection()
+            .db('flavor-hub')
+            .collection('modification')
+            .find({modified_type: typeId});
+        result.toArray().then((lists) => 
+        {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists[0]);
+        });
+    }
 }
 
 module.exports = 
