02d1229608a32f188dd8fe25246677978017161b
quenten8 <quenten.cox00@outlook.com>
2023-07-15 23:08:11 -0700

    Fixed Swagger routes

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -2,9 +2,9 @@ const passport = require('passport');
 
 // Check authentication
 const ensureAuth = (req, res, next) => {
-        // #swagger.tags = ['Authentication']
-        // #swagger.summary = 'Ensure authentication'
-        // #swagger.description = 'This route ensures that the user is authenticated'
+  // #swagger.tags = ['Authentication']
+  // #swagger.summary = 'Ensure authentication'
+  // #swagger.description = 'This route ensures that the user is authenticated'
   if (req.isAuthenticated()) {
     return next();
   } else {
@@ -13,9 +13,9 @@ const ensureAuth = (req, res, next) => {
 };
 
 const ensureGuest = (req, res, next) => {
-          // #swagger.tags = ['Authentication']
-        // #swagger.summary = 'Ensure guest'
-        // #swagger.description = 'This route ensures that the user is not authenticated'
+  // #swagger.tags = ['Authentication']
+  // #swagger.summary = 'Ensure guest'
+  // #swagger.description = 'This route ensures that the user is not authenticated'
   if (req.isAuthenticated()) {
     res.redirect('/auth/dashboard');
   } else {
@@ -28,9 +28,9 @@ const authenticateWithGoogle = passport.authenticate('google', { scope: ['profil
 
 // Google authentication callback
 const googleCallback = (req, res, next) => {
-          // #swagger.tags = ['Authentication']
-        // #swagger.summary = 'Google OAuth callback'
-        // #swagger.description = 'This is a callback route for logging in with Google OAuth'
+  // #swagger.tags = ['Authentication']
+  // #swagger.summary = 'Google OAuth callback'
+  // #swagger.description = 'This is a callback route for logging in with Google OAuth'
   passport.authenticate('google', { failureRedirect: '/' }, (err, user, info) => {
     if (err) { return next(err); }
     if (!user) { return res.redirect('/'); }
@@ -42,14 +42,14 @@ const googleCallback = (req, res, next) => {
       return res.redirect('/auth/dashboard');
     });
   })(req, res, next);
-  
+
 };
 
 // Logout
 const logout = (req, res, next) => {
-          // #swagger.tags = ['Authentication']
-        // #swagger.summary = 'Logout'
-        // #swagger.description = 'This route will log the user out'
+  // #swagger.tags = ['Authentication']
+  // #swagger.summary = 'Logout'
+  // #swagger.description = 'This route will log the user out'
   req.logout(function (err) {
     if (err) return next(err);
     res.redirect('/');
