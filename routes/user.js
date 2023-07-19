f583ef170cc9c9b6df1989ab17ef38cfeccd0df6
quenten8 <quenten.cox00@outlook.com>
2023-07-19 13:28:04 -0700

    Fixed swagger not working on render

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,12 @@ const usersController = require('../controllers/user')
 const {  ensureAuth } = require('../controllers/authentication');
 const {validateNewUser, validateUserId, validateUpdateUser} = require('../validation/validateUser')
 
-router.get('/:userId', validateUserId(), usersController.getUserByUserId);
+router.get('/:userId', ensureAuth, validateUserId(), usersController.getUserByUserId);
 
-router.post('/', validateNewUser(), usersController.createNewUser);
+router.post('/', ensureAuth, validateNewUser(), usersController.createNewUser);
 
-router.put('/:userId', validateUpdateUser(), usersController.updateUser);
+router.put('/:userId', ensureAuth, validateUpdateUser(), usersController.updateUser);
 
-router.delete('/:userId', usersController.deleteUser);
+router.delete('/:userId', ensureAuth, usersController.deleteUser);
 
 module.exports = router;
